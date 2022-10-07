119ca413660e730de8d1c77f3d505103d2f2d930
Martin Holst Swende <martin@swende.se>
2022-10-07 12:00:16 +0200

    use ioutil.readdir for backwards compatibility

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"time"
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
@@ -18,7 +19,8 @@ func scan(keyDir string) error {
 	fmt.Printf("1. Scanning directory %v...\n", keyDir)
 	// List all the files from the keystore folder
 	t0 := time.Now()
-	files, err := os.ReadDir(keyDir)
+	files, err := ioutil.ReadDir(keyDir)
+	//files, err := os.ReadDir(keyDir) <-- not available pre 1.16
 	if err != nil {
 		fmt.Printf("ERROR: Scan error after %v : %v\n", time.Since(t0), err)
 		return err
@@ -26,12 +28,13 @@ func scan(keyDir string) error {
 	fmt.Printf("   Scan ok, %d files, elapsed %v\n", len(files), time.Since(t0))
 	fmt.Printf("2. Iterating over files...\n")
 	for _, fi := range files {
-		info, err := fi.Info()
-		if err != nil {
-			fmt.Printf("ERROR: Err while getting file info for %v: %v\n", fi.Name(), err)
-			return err
-		}
-		info.ModTime()
+		fi.ModTime()
+		//info, err := fi.Info()
+		//if err != nil {
+		//	fmt.Printf("ERROR: Err while getting file info for %v: %v\n", fi.Name(), err)
+		//	return err
+		//}
+		//info.ModTime()
 	}
 	fmt.Printf("   Iteration done, elapsed %v\n", time.Since(t0))
 	return nil
