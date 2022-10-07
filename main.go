29d4d0686daab7576fac8360afc17d6f9a26c2fe
Martin Holst Swende <martin@swende.se>
2022-10-07 11:45:47 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"time"
+	"fmt"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: filescan <path>\n")
+		os.Exit(1)
+	}
+	scan(os.Args[1])
+}
+
+func scan(keyDir string) error {
+	fmt.Printf("1. Scanning directory %v...\n", keyDir)
+	// List all the files from the keystore folder
+	t0 := time.Now()
+	files, err := os.ReadDir(keyDir)
+	if err != nil {
+		fmt.Printf("ERROR: Scan error after %v : %v\n", time.Since(t0), err)
+		return err
+	}
+	fmt.Printf("   Scan ok, %d files, elapsed %v\n", len(files), time.Since(t0))
+	fmt.Printf("2. Iterating over files...\n")
+	for _, fi := range files {
+		info, err := fi.Info()
+		if err != nil {
+			fmt.Printf("ERROR: Err while getting file info for %v: %v\n", fi.Name(), err)
+			return err
+		}
+		info.ModTime()
+	}
+	fmt.Printf("   Iteration done, elapsed %v\n", time.Since(t0))
+	return nil
+}
